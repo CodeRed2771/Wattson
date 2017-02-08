b7bde556d2ecc9960a2adf31fa9ea227926eb132
Cymbre <cymbre@mattscott.org>
2017-02-07 21:48:07 -0500

    Added some things to Shooter

--- src/com/coderedrobotics/Shooter.java
+++ src/com/coderedrobotics/Shooter.java
@@ -8,15 +8,24 @@ public class Shooter {
 	CANTalon shooter;
 	PIDControllerAIAO pid;
 
-
-
-	public void spinUpShooter(){
-	}
-	
-	public void stopShooter(){
-	}
-	
-	public void feedShooter(){
+	public Shooter() {
+		ballFeeder = new CANTalon(Wiring.SHOOTER_MOTOR_FEEDER);
+		shooter = new CANTalon(Wiring.SHOOTER_MOTOR_SHOOTER);
+		shooter.setPID(Calibration2016.SHOOTER_P, Calibration2016.SHOOTER_I, Calibration2016.SHOOTER_D);
 	}
 
+	public void spinUpShooter() {
+		
+	}
+
+	public boolean isSpunUp() {
+		return Math.abs(shooter.getClosedLoopError()) < 20;
+	}
+
+	public void stopShooter() {
+		
+	}
+
+	public void feedShooter() {
+	}
 }
