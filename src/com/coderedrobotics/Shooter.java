b1c4b507fed401b68c32cc75ab77b91338cb574a
Cymbre <cymbre@mattscott.org>
2017-02-07 19:23:58 -0500

    Added a shooter class

---
+++ src/com/coderedrobotics/Shooter.java
@@ -0,0 +1,10 @@
+package com.coderedrobotics;
+
+import com.coderedrobotics.libs.PIDControllerAIAO;
+import com.ctre.CANTalon;
+
+public class Shooter {
+	CANTalon ballFeeder;
+	CANTalon shooter;
+	PIDControllerAIAO pid;
+}
