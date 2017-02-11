48d2ed22da2b8eae5732572f2d58c138fc79cd81
Cymbre <cymbre@mattscott.org>
2017-02-11 14:45:51 -0500

    Master/Slave Shooter Class

--- src/com/coderedrobotics/Shooter.java
+++ src/com/coderedrobotics/Shooter.java
@@ -8,39 +8,37 @@ import com.ctre.CANTalon.TalonControlMode;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter {
-	CANTalon ballFeeder;
+	CANTalon shooterFollower;
 	CANTalon shooter;
 	PIDControllerAIAO pid;
 
 	public Shooter() {
-		ballFeeder = new CANTalon(Wiring.SHOOTER_MOTOR_FEEDER);
 		shooter = new CANTalon(Wiring.SHOOTER_MOTOR_SHOOTER);
 		shooter.setPID(Calibration2016.SHOOTER_P, Calibration2016.SHOOTER_I, Calibration2016.SHOOTER_D);
 		shooter.setFeedbackDevice(FeedbackDevice.QuadEncoder);
-		ballFeeder.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+
+		shooterFollower = new CANTalon(3);
+		shooterFollower.changeControlMode(CANTalon.TalonControlMode.Follower);
+		shooterFollower.set(2);
 
 		shooter.reverseSensor(true);
-		ballFeeder.reverseSensor(true);
+		shooterFollower.reverseSensor(true);
 
-		shooter.configNominalOutputVoltage(0,6);
-		ballFeeder.configNominalOutputVoltage(0,6);
-		
+		shooter.configNominalOutputVoltage(0, 6);
+		shooterFollower.configNominalOutputVoltage(0, 6);
+
 		shooter.configEncoderCodesPerRev(0);
-		ballFeeder.configEncoderCodesPerRev(0);
 
 		shooter.setProfile(0);
 		shooter.setPID(0, 0, 0);
 		shooter.changeControlMode(TalonControlMode.Speed);
-		
-		ballFeeder.setProfile(0);
-		ballFeeder.setPID(0, 0, 0);
-		ballFeeder.changeControlMode(TalonControlMode.Speed);
+
 		SmartDashboard.putNumber("Shooter Setpoint", 0);
 		SmartDashboard.putNumber("Set P", 0);
 		SmartDashboard.putNumber("Set I", 0);
 		SmartDashboard.putNumber("Set D", 0);
 		SmartDashboard.putNumber("Set F", 0);
-		
+
 	}
 
 	public void spinUpShooter() {
@@ -61,6 +59,6 @@ public class Shooter {
 	}
 
 	public void feedShooter() {
-		ballFeeder.setSetpoint(SmartDashboard.getNumber("Ball Feeder Setpoint", 0));
+		shooterFollower.setSetpoint(SmartDashboard.getNumber("Ball Feeder Setpoint", 0));
 	}
 }
