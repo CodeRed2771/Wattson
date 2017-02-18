c7f5d1ecb11a6197b0bc39a6a77db729011af615
Dale Van Voorst <dale@dalevanvoorst.com>
2017-02-17 21:55:22 -0500

    updated pickup

--- src/com/coderedrobotics/Pickup.java
+++ src/com/coderedrobotics/Pickup.java
@@ -6,27 +6,34 @@ import com.coderedrobotics.libs.PWMController;
 public class Pickup {
 
 	private final PWMController sweeperMotor;
-	private boolean pickingup = false;
-
+	private boolean pickingUp = false;
+	
 	public Pickup() {
 		sweeperMotor = new PWMController(Wiring.SWEEPER_MOTOR, false);
 	}
 
 	public void sweeperStart() {
 		sweeperMotor.set(1);
-
+		pickingUp = true;
 	}
-
-	public void sweeperReverse() {
-		sweeperMotor.set(-1);
-
-	}
-
+	
 	public void sweeperStop() {
 		sweeperMotor.set(0);
+		pickingUp = false;
+	}
+	
+	public void sweeperReverse() {
+		sweeperMotor.set(-1);
+	}
 
+	public void toggle() {
+		if (pickingUp) {
+			sweeperStop();
+		} else
+			sweeperStart();
 	}
 
 	public void tick() {
+		
 	}
 }
