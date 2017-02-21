bcb9989e02dd75e2dfc375991558dae64e30a4bd
Dale Van Voorst <dale@dalevanvoorst.com>
2017-02-20 23:03:48 -0500

    wiring changes

--- src/com/coderedrobotics/Wiring.java
+++ src/com/coderedrobotics/Wiring.java
@@ -3,16 +3,16 @@ package com.coderedrobotics;
 public class Wiring {
 
 	// PWM MOTOR CONTROLLERS
-	public static final int LEFT_DRIVE_MOTOR1 = 0;
-	public static final int LEFT_DRIVE_MOTOR2 = 1;
-	public static final int RIGHT_DRIVE_MOTOR1 = 2;
-	public static final int RIGHT_DRIVE_MOTOR2 = 3;
-	public static final int GEAR_RECEIVER_MOTOR = 10;
-	public static final int GEAR_PICKUP_FINGERS = 5;
-	public static final int AGITATOR_TOP_MOTOR = 6;
-	public static final int AGITATOR_BOTTOM_MOTOR1 = 7;
-	public static final int AGITATOR_BOTTOM_MOTOR2 = 8;
-	public static final int CLIMBER_MOTOR = 9;
+	public static final int LEFT_DRIVE_MOTOR1 = 8;
+	public static final int LEFT_DRIVE_MOTOR2 = 9;
+	public static final int RIGHT_DRIVE_MOTOR1 = 6;
+	public static final int RIGHT_DRIVE_MOTOR2 = 7;
+	public static final int GEAR_RECEIVER_MOTOR = 2;
+	public static final int GEAR_PICKUP_FINGERS = 1;
+	public static final int AGITATOR_TOP_MOTOR = 3;
+	public static final int AGITATOR_BOTTOM_MOTOR1 = 4;
+	public static final int AGITATOR_BOTTOM_MOTOR2 = 5;
+	public static final int CLIMBER_MOTOR = 0;
 	
 	// MOTOR CONTROLLER PDP PORTS
 	public static final int PICKUP_FRONT_PDP = 10;
@@ -33,11 +33,10 @@ public class Wiring {
 	public static final int SWEEPER_MOTOR_FOLLOWER = 8;
 
 	// DIGITAL INPUT
-	public static final int ARM_LIMIT_SWITCH = 0;
-	public static final int LEFT_ENCODER_A = 2;
+	public static final int LEFT_ENCODER_A = 0;
 	public static final int LEFT_ENCODER_B = 1;
-	public static final int RIGHT_ENCODER_A = 3;
-	public static final int RIGHT_ENCODER_B = 4;
+	public static final int RIGHT_ENCODER_A = 2;
+	public static final int RIGHT_ENCODER_B = 3;
 
 	// ANALOG INPUT
 	public static final int GEAR_IR_SENSOR = 0;
