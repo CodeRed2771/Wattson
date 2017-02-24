572429418617743aac53db445069a11224c7ef63
Cymbre <cymbre@mattscott.org>
2017-02-23 22:02:26 -0500

    Auto Peg Dead Reconing

--- src/com/coderedrobotics/AutoPegCS.java
+++ src/com/coderedrobotics/AutoPegCS.java
@@ -11,9 +11,38 @@ public class AutoPegCS extends AutoBaseClass {
 	public void tick() {
 		if (isRunning()) {
 			switch (getCurrentStep()) {
-			case 0:
+			case 0: //position 1
+				setTimerAndAdvanceStage(5000);
+				driveInches(54, .5); //TODO hone in the distance
+				break;
+			case 1: 
+				if(driveAuto().hasArrived())
+					advanceStage();
+					break;
+			case 2:
 				setTimerAndAdvanceStage(3000);
-
+					turnDegrees(.45, .2);
+					break;
+			case 3:
+				if(driveAuto().hasArrived())
+					advanceStage();
+					break;
+			case 4:
+				setTimerAndAdvanceStage(3000);
+				driveInches(20, .3);
+				break;
+			case 5:
+				if(driveAuto().hasArrived())
+					advanceStage();
+				break;
+			case 6:
+				setTimerAndAdvanceStage(3000);
+				driveInches(-5000, .5);
+				break;
+			case 7:
+				if(driveAuto().hasArrived())
+					advanceStage();
+					break;
 			}
 		}
 	}
