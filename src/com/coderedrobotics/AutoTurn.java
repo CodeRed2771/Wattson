3e5a5386426c68931f5e9564dec8b137e7a19756
Dale Van Voorst <dale@dalevanvoorst.com>
2017-03-04 11:39:48 -0500

    a

---
+++ src/com/coderedrobotics/AutoTurn.java
@@ -0,0 +1,35 @@
+package com.coderedrobotics;
+
+import com.coderedrobotics.libs.AutoBaseClass;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class AutoTurn extends AutoBaseClass {
+
+	public AutoTurn(DriveAuto driveAuto, int robotPosition) {
+		super(driveAuto, robotPosition);
+	}
+
+	public void tick() {
+		
+		if (isRunning()) {
+			this.driveAuto().showEncoderValues();
+			
+			switch (getCurrentStep()) {
+			case 0:
+				setTimerAndAdvanceStage(5000);
+				turnDegrees(180, .6);
+				break;
+			case 1:
+				if (driveAuto().hasArrived())
+					advanceStage();
+				break;
+			case 2:
+				stop();
+				break;
+			}
+		}
+
+	}
+
+}
