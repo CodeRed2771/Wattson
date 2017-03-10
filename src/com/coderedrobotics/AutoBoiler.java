45e5f745e4830cb92d416926e985ebcb67ccf74b
Ruoxi <ruoxi17macaron@gmail.com>
2017-03-09 20:58:42 -0500

    Update AutoBoiler class

--- src/com/coderedrobotics/AutoBoiler.java
+++ src/com/coderedrobotics/AutoBoiler.java
@@ -11,13 +11,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 */
 public class AutoBoiler extends AutoBaseClass {
 	Target target;
+	Shooter shooter;
 	boolean foundTarget = false;
-	
-	public AutoBoiler(DriveAuto driveAuto, int robotPosition, Target target) {
+
+	public AutoBoiler(DriveAuto driveAuto, int robotPosition, Target target, Shooter shooter) {
 		super(driveAuto, robotPosition);
 		this.target = target;
+		this.shooter = shooter;
 	}
-	
+
 	public void tick() {
 
 		if (isRunning()) {
@@ -25,87 +27,86 @@ public class AutoBoiler extends AutoBaseClass {
 			case 0:
 				// align to the peg target
 				target.enableVisionTargetMode(true, "Boiler");
-				//setTimerAndAdvanceStage(2000);
+				// setTimerAndAdvanceStage(2000);
 				break;
-				
+
 			case 1:
 				foundTarget = target.foundBoilerTarget();
 				if (foundTarget) {
 					advanceStage();
-				}else{
+				} else {
 					setCurrentStep(1000);
 				}
 				break;
-				
+
 			case 2:
 				if (foundTarget) {
 					setTimerAndAdvanceStage(2000);
 					turnDegrees(target.getBoilerAngle(), 1);
-				} 
-				else {
+				} else {
 					// we just sit here because we can't see the target
 				}
 				break;
-				
-			case 3: 
+
+			case 3:
 				if (driveAuto().hasArrived()) // done aligning
 					advanceStage();
 				break;
-				
+
 			case 4:
 				// drive toward peg target and stop 10" short
 				setTimerAndAdvanceStage(5000);
-				driveInches(80, .20);//80 in is about the half way from gear target to boiler
+				driveInches(80, .20);// 80 in is about the half way from gear
+										// target to boiler
+				shooter.spinUpShooter();
 				break;
-				
-			case 5: 
-				if (driveAuto().hasArrived()) 
+
+			case 5:
+				if (driveAuto().hasArrived())
 					advanceStage();
 				break;
 
 			case 6:
+				shooter.feedShooter();
 				target.enableVisionTargetMode(false, "");
-				//stop();
+				setTimerAndAdvanceStage(5000);
+				break;
+				
+			case 7:
+				break;
+				
+			case 8:
+				shooter.stopShooter();
+				advanceStage();
+				break;
+				
+			case 9:
+				target.enableVisionTargetMode(false, "");
 				break;
 			}
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//SmartDashboard.putBoolean("IsDriving", isDriving());
-//		driveAuto.showEncoderValues();
-//		
-//		//update the pid values based on numbers entered into the SmartDashboard
-//		driveAuto.updatePIDValues();
-//		target.displayDetails();
-//		
-//		if (!isDriving && target.foundTarget() && !target.isOnTarget()){
-//			driveAuto.turnDegrees(-target.degreesOffTarget(), 1);
-//			
-//			isDriving = true;
-//		}
-//		
-//		if (!isDriving && target.isOnTarget() && target.foundTarget()) {
-//			driveAuto.driveInches(target.distanceFromGearTarget() - 10, .20);
-//			isDriving = true;
-//		}
-//		
-//		if (driveAuto.hasArrived()) {
-//			isDriving = false;
-//		}
+
+		// SmartDashboard.putBoolean("IsDriving", isDriving());
+		// driveAuto.showEncoderValues();
+		//
+		// //update the pid values based on numbers entered into the
+		// SmartDashboard
+		// driveAuto.updatePIDValues();
+		// target.displayDetails();
+		//
+		// if (!isDriving && target.foundTarget() && !target.isOnTarget()){
+		// driveAuto.turnDegrees(-target.degreesOffTarget(), 1);
+		//
+		// isDriving = true;
+		// }
+		//
+		// if (!isDriving && target.isOnTarget() && target.foundTarget()) {
+		// driveAuto.driveInches(target.distanceFromGearTarget() - 10, .20);
+		// isDriving = true;
+		// }
+		//
+		// if (driveAuto.hasArrived()) {
+		// isDriving = false;
+		// }
 	}
 }
